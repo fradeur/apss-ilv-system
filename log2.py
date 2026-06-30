4d410539e1e7d517aae39e725b772c72cee74947
fradeur <107208607+fradeur@users.noreply.github.com>
2026-06-30 13:50:14 +0700

    Update API key in log2.py

--- log2.py
+++ log2.py
@@ -6,7 +6,7 @@ import json
 # ==========================================
 # SYSTEM CONFIGURATION
 # ==========================================
-client = genai.Client(api_key="GEMINI_API_KEY")
+client = genai.Client(api_key="KEY")
 st.set_page_config(page_title="APSS - Advanced Audit", layout="wide")
 
 
